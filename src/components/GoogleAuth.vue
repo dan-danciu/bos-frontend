4783b213b753d43456ccc54623f1d353373901e2
dan-danciu <dan.danciu13@gmail.com>
2018-11-21 23:29:33 +0200

    tiny mistake.. ehem

--- src/components/GoogleAuth.vue
+++ src/components/GoogleAuth.vue
@@ -45,8 +45,8 @@ export default {
             vm.id_token = googleUser.getAuthResponse().id_token;
             //vm.$emit('authenticated', {'id_token':vm.id_token, 'auth':vm.auth2});
             //eventBus.$emit('authenticated', {'id_token':vm.id_token, 'auth2':vm.auth2});
-            this.setToken(vm.id_token);
-            this.createAuth2(vm.auth2);
+            vm.setToken(vm.id_token);
+            vm.createAuth2(vm.auth2);
             //vm.authenticate();
           }, function(error) {
             alert(JSON.stringify(error, null, 2));
