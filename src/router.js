6ac151456efc0a146e7df60a3b084c81ae6eb1f4
dan-danciu <dan.danciu13@gmail.com>
2018-11-25 20:03:43 +0200

    back to store token

--- src/router.js
+++ src/router.js
@@ -10,7 +10,7 @@ router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
     // this route requires auth, check if logged in
     // if not, redirect to login page.
-    if (!localStorage.getItem('token')) {
+    if (!store.state.auth.id_token) {
       next({
         path: '/login'
         //query: { redirect: to.fullPath }
